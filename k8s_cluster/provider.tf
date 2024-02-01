353bed17bb3b216b389a7461747b33ef57da242e
Sorav Kumar Sharma <add.sorav@gmail.com>
2024-02-01 13:54:36 +0530

    update terraform script

--- k8s_cluster/provider.tf
+++ k8s_cluster/provider.tf
@@ -5,11 +5,6 @@ terraform {
       version = "5.34.0"
     }
   }
-  backend "s3" {
-    bucket = "soravks-tictactoe-gitops"
-    region = "us-east-1"
-    key    = "prod/tictactoe/state/terraform.tfstate"
-  }
 }
 
 provider "aws" {
